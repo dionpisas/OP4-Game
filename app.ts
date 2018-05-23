35756fe7faad08f6d19f9a0a37db169b6e080724
Amy Geelhoed <0933602@hr.nl>
2018-05-23 16:02:26 +0200

    Added Main game with sun and a couple planets thogeter with player and player controls

--- app.ts
+++ app.ts
@@ -1,25 +1,202 @@
 ﻿/// <reference path="phaser/phaser.d.ts" />
-class SimpleGame {
+
+class Game {
+    game: Phaser.Game
+    star: Phaser.Sprite
+    Sprite: Phaser.Sprite
+    spaceshipSprite: Phaser.Sprite
+    earthSprite: Phaser.Sprite
+    jupiterSprite: Phaser.Sprite
+    Cursors: Phaser.CursorKeys
+    Keyboard: Phaser.Keyboard
+    uiSprite: Phaser.Sprite
 
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'content', { create: this.create, preload: this.preload, update: this.update, render: this.render })
     }
-
-    game: Phaser.Game;
-
     preload() {
-        this.game.load.image('logo', 'phaser1.png');
-    }
+        //Loads sprite images
 
+        let sunLoader = this.game.load.image("sun", "Assets/Graphics/sun.png")
+        let earthLoader = this.game.load.image("earth", "Assets/Graphics/earth.png")
+        let uiLoader = this.game.load.image("ui", "Assets/Graphics/ui.png")
+        let jupiterLoader = this.game.load.image("jupiter", "Assets/Graphics/jupiter.png")
+        let spaceshipLoader = this.game.load.atlasXML("spaceship", "Assets/Graphics/sheet.png", "Assets/Graphics/sheet.xml")
+        let starLoader = this.game.load.image("star","Assets/Graphics/star.png")
+
+    }
     create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-    }
 
+        this.game.physics.startSystem(Phaser.Physics.P2JS)
+        this.game.world.setBounds(0, 0, 3000, 3000);
+        //adds the image to a variable
+     
+
+        
+
+        //adds sprites to the game and sets coordinates
+        
+        for (let i = 0; i < 200; i++) {
+            this.star = this.game.add.sprite(this.game.world.randomX, this.game.world.randomY, "star")
+           
+        }
+        this.Sprite = this.game.add.sprite(this.game.world.centerX - 175, this.game.world.centerY - 175, "sun")
+        this.earthSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "earth")
+        this.jupiterSprite = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY, "jupiter")
+        this.uiSprite = this.game.add.sprite(0, 40, "ui")
+        this.spaceshipSprite = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY, "spaceship")
+        this.game.physics.p2.enable(this.spaceshipSprite)
+
+        //scale sprites down 
+        this.jupiterSprite.scale.set(0.2, 0.2)
+        this.earthSprite.scale.set(0.5, 0.5)
+        this.spaceshipSprite.scale.set(0.3, 0.3)
+        this.Sprite.scale.set(0.5,0.5)
+     
+        this.spaceshipSprite.frame = 218
+       // this.earthSprite.pivot.x = 1200
+        //  this.jupiterSprite.pivot.x = 1200  
+        this.star.pivot.x = 1200
+        this.uiSprite.fixedToCamera = true;
+        //add keyboard controls
+        this.Cursors = this.game.input.keyboard.createCursorKeys();
+        this.Keyboard = this.game.input.keyboard
+        this.game.camera.follow(this.spaceshipSprite);
+    }
+    update() {
+
+        //sets keyboard controls
+      this.spaceshipSprite.body.setZeroVelocity()
+        this.game.input.update()
+        if (this.Cursors.up.isDown) {
+            this.spaceshipSprite.body.moveUp(300)
+          
+        }
+        else if (this.Cursors.down.isDown) {
+            this.spaceshipSprite.body.moveDown(300);
+            
+        }
+        if (this.Cursors.left.isDown) {
+            this.spaceshipSprite.body.velocity.x = -300;
+        }
+        else if (this.Cursors.right.isDown) {
+            this.spaceshipSprite.body.moveRight(300);
+        }
+        this.spaceshipSprite.rotation += 0.5
+        this.earthSprite.rotation += 0.0010;
+        this.jupiterSprite.rotation += 0.0020;
+
+        if (this.Keyboard.addKey(32).isDown) {
+            
+            this.earthSprite.rotation += 0.010;
+            this.jupiterSprite.rotation += 0.020;
+         
+        
+        }
+        
+    }
+    render() {
+        //this.game.debug.cameraInfo(this.game.camera, 32, 32)
+        //this.game.debug.spriteCoords(this.spaceshipSprite, 32, 500)
+    }
 }
 
 window.onload = () => {
+    let game = new Game()
+}
 
-    var game = new SimpleGame();
 
+
+
+/*
+module Game {
+    export class TitleScreenState extends Phaser.State {
+        game: Phaser.Game
+        constructor() {
+            super()
+        }
+        sprites: Phaser.Sprite
+
+        preload() {
+            this.load.image("title", "Assets/Graphics/TitleScreen.png")
+            this.load.image("earth", "Assets/Graphics/earth_low.png")
+            this.load.image("sun", "Assets/Graphics/sun.png")
+            this.load.image("mars", "Assets/Graphics/mars.png")
+            this.load.image("venus", "Assets/Graphics/venus.png")
+            this.load.image("jupiter", "Assets/Graphics/jupiter.png")
+        }
+        create() {
+            this.sprites = this.add.sprite(0, 0, "title")
+            this.input.onTap.addOnce(this.titleClicked, this) // <-- that um, this is extremely important
+        }
+        titleClicked() {
+            this.game.state.start("SpaceshipScreenState")
+        }
+    }
+    export class SpaceshipScreenState extends Phaser.State {
+        game: Phaser.Game
+        constructor() {
+            super()
+       }
+        spaceshipImage: Phaser.Sprite
+
+        preload() {
+            this.load.image("spaceship1", "Assets/Graphics/Spaceship1.png")
+            this.load.image("spaceship2", "Assets/Graphics/Spaceship2.png")
+            this.load.image("spaceship3", "Assets/Graphics/Spaceship3.png")
+        }
+        create() {
+            this.spaceshipImage = this.add.sprite(300, 300, "spaceship1")
+            this.spaceshipImage = this.add.sprite(500, 300, "spaceship2")
+            this.spaceshipImage = this.add.sprite(700, 300, "spaceship3")
+            this.input.onTap.addOnce(this.spaceshipClicked, this) // <-- that um, this is extremely important
+        }
+        spaceshipClicked() {
+            this.game.state.start("GameRunningState")
+            this.spaceshipImage.x++
+        }
+    }
+    export class GameRunningState extends Phaser.State {
+        constructor() {
+            super()
+        }
+      sprites: Phaser.Sprite
+
+        create() {
+            
+            this.sprites = this.add.sprite(600, 300, "earth")
+            this.sprites = this.add.sprite(350, 250, "mars")
+            this.sprites = this.add.sprite(1090, 350, "venus")
+            this.sprites = this.add.sprite(0, 400, "jupiter")
+            this.sprites = this.add.sprite(900, -300, "sun")
+        }
+
+        update() {
+            
+            
+        }
+
+        render() {
+            
+        }
+    }
+
+    export class SpaceGame {
+        game: Phaser.Game
+
+        constructor() {
+            this.game = new Phaser.Game(1280,720, Phaser.WEBGL, 'content')
+
+            this.game.state.add("GameRunningState", GameRunningState, false)
+            this.game.state.add("TitleScreenState", TitleScreenState, false)
+            this.game.state.add("SpaceshipScreenState", SpaceshipScreenState, false)
+            this.game.state.start("TitleScreenState", true, true)
+        }
+
+    }
+}
+
+window.onload = () => {
+    var game = new Game.SpaceGame()
 };
+*/
