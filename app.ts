aa0e712025f85f80d070498dfd2956c2ea1ecda5
Amy Geelhoed <0933602@hr.nl>
2018-05-28 11:42:22 +0200

    backup commit

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-﻿/// <reference path="phaser/phaser.d.ts" />
+﻿
+/// <reference path="phaser/phaser.d.ts" />
 
 class Game {
     game: Phaser.Game
@@ -6,6 +7,8 @@ class Game {
     Sprite: Phaser.Sprite
     spaceshipSprite: Phaser.Sprite
     earthSprite: Phaser.Sprite
+    marsSprite: Phaser.Sprite
+    venusSprite: Phaser.Sprite
     jupiterSprite: Phaser.Sprite
     Cursors: Phaser.CursorKeys
     Keyboard: Phaser.Keyboard
@@ -22,12 +25,15 @@ class Game {
         let uiLoader = this.game.load.image("ui", "Assets/Graphics/ui.png")
         let jupiterLoader = this.game.load.image("jupiter", "Assets/Graphics/jupiter.png")
         let spaceshipLoader = this.game.load.atlasXML("spaceship", "Assets/Graphics/sheet.png", "Assets/Graphics/sheet.xml")
-        let starLoader = this.game.load.image("star","Assets/Graphics/star.png")
+        let starLoader = this.game.load.image("star", "Assets/Graphics/star.png")
+        let marsLoader = this.game.load.image("mars", "Assets/Graphics/mars.png")
+        let venusLoader = this.game.load.image("venus","Assets/Graphics/venus.png")
 
     }
     create() {
 
         this.game.physics.startSystem(Phaser.Physics.P2JS)
+        
         this.game.world.setBounds(0, 0, 3000, 3000);
         //adds the image to a variable
      
@@ -36,32 +42,52 @@ class Game {
 
         //adds sprites to the game and sets coordinates
         
-        for (let i = 0; i < 200; i++) {
+        for (let i = 0; i < 300; i++) {
             this.star = this.game.add.sprite(this.game.world.randomX, this.game.world.randomY, "star")
            
         }
         this.Sprite = this.game.add.sprite(this.game.world.centerX - 175, this.game.world.centerY - 175, "sun")
         this.earthSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "earth")
-        this.jupiterSprite = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY, "jupiter")
+        this.jupiterSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "jupiter")
+        this.marsSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "mars")
+        this.venusSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "venus")
         this.uiSprite = this.game.add.sprite(0, 40, "ui")
         this.spaceshipSprite = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY, "spaceship")
         this.game.physics.p2.enable(this.spaceshipSprite)
+        this.game.physics.p2.enable(this.earthSprite)
+        
 
         //scale sprites down 
         this.jupiterSprite.scale.set(0.2, 0.2)
-        this.earthSprite.scale.set(0.5, 0.5)
-        this.spaceshipSprite.scale.set(0.3, 0.3)
+        this.earthSprite.scale.set(0.3, 0.3)
+        this.marsSprite.scale.set(0.3, 0.3)
+        this.venusSprite.scale.set(0.15,0.15)
+        this.spaceshipSprite.scale.set(0.4, 0.5)
         this.Sprite.scale.set(0.5,0.5)
      
         this.spaceshipSprite.frame = 218
-       // this.earthSprite.pivot.x = 1200
-        //  this.jupiterSprite.pivot.x = 1200  
+        this.earthSprite.pivot.x = 2000
+        this.marsSprite.pivot.x = 2500
+        this.venusSprite.pivot.x = 2700
+        this.jupiterSprite.pivot.x = 4500  
+        this.spaceshipSprite.pivot.x = 2000
         this.star.pivot.x = 1200
         this.uiSprite.fixedToCamera = true;
         //add keyboard controls
         this.Cursors = this.game.input.keyboard.createCursorKeys();
         this.Keyboard = this.game.input.keyboard
         this.game.camera.follow(this.spaceshipSprite);
+        let playerCollisionGroup = this.game.physics.p2.createCollisionGroup
+        let planetCollisionGroup = this.game.physics.p2.createCollisionGroup
+        this.spaceshipSprite.body.setCollisionGroup(playerCollisionGroup)
+        this.earthSprite.body.setCollisionGroup(planetCollisionGroup)
+        this.spaceshipSprite.physicsType = Phaser.Physics.P2JS  
+        this.spaceshipSprite.body.collides([planetCollisionGroup, playerCollisionGroup])
+        this.spaceshipSprite.body.collides(planetCollisionGroup, this.log, this)
+
+    }
+    log() {
+        console.log(123)
     }
     update() {
 
@@ -69,36 +95,45 @@ class Game {
       this.spaceshipSprite.body.setZeroVelocity()
         this.game.input.update()
         if (this.Cursors.up.isDown) {
-            this.spaceshipSprite.body.moveUp(300)
+            this.spaceshipSprite.body.velocity.y = -300
           
         }
         else if (this.Cursors.down.isDown) {
-            this.spaceshipSprite.body.moveDown(300);
+            this.spaceshipSprite.body.velocity.y = 300;
             
         }
         if (this.Cursors.left.isDown) {
             this.spaceshipSprite.body.velocity.x = -300;
         }
         else if (this.Cursors.right.isDown) {
-            this.spaceshipSprite.body.moveRight(300);
+            this.spaceshipSprite.body.velocity.x = 300;
         }
         this.spaceshipSprite.rotation += 0.5
         this.earthSprite.rotation += 0.0010;
         this.jupiterSprite.rotation += 0.0020;
+        this.marsSprite.rotation += 0.0013
+        this.venusSprite.rotation += 0.0013
+        
 
         if (this.Keyboard.addKey(32).isDown) {
             
             this.earthSprite.rotation += 0.010;
-            this.jupiterSprite.rotation += 0.020;
-         
-        
+            this.jupiterSprite.rotation += 0.020;        
         }
-        
+
+        let spaceshipBounds = this.spaceshipSprite.getBounds()
+        let earthBounds = this.earthSprite.getBounds()
+        if (this.spaceshipSprite.body.collides(this.earthSprite,this.earthSprite)) {
+        console.log(123)}
+   
     }
     render() {
-        //this.game.debug.cameraInfo(this.game.camera, 32, 32)
-        //this.game.debug.spriteCoords(this.spaceshipSprite, 32, 500)
+        this.game.debug.body(this.earthSprite)
+        this.game.debug.cameraInfo(this.game.camera, 32, 32)
+        this.game.debug.spriteCoords(this.spaceshipSprite, 32,500)
+        this.game.debug.spriteCoords(this.earthSprite, 32, 200)
     }
+
 }
 
 window.onload = () => {
